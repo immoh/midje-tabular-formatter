6720e3ce13e4cd766f1a4f7823f1434231139d42
Immo Heikkinen <immo.heikkinen@zalando.fi>
2019-12-04 10:43:06 +0200

    Bump to next development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject midje-tabular-formatter "0.1.0"
+(defproject midje-tabular-formatter "0.2.0-SNAPSHOT"
   :description "Tool for formatting Midje tabular tests"
   :url "http://github.com/immoh/midje-tabular-formatter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
